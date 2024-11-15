104b5a82ec10409c324ce78e041b6fe437b43f6d
Fei Zhan <fei.zhan.zju@gmail.com>
2024-11-14 17:32:19 -0800

    Add Tooltip

--- client/src/components/CovidWorldMap.tsx
+++ client/src/components/CovidWorldMap.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
 import { scaleLinear } from 'd3-scale';
+import { Tooltip, Typography } from "@mui/material";
+import { CovidData, CovidCountryData } from '../types/CovidData'; // Import types
 
 // Define color scale for cases
 const colorScale = scaleLinear<string>()
@@ -17,6 +19,8 @@ interface CovidWorldMapProps {
 
 const CovidWorldMap: React.FC<CovidWorldMapProps> = ({ data }) => {
   const [countryData, setCountryData] = useState<{ [key: string]: number }>({});
+  const [tooltipContent, setTooltipContent] = useState("");
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   // Map dataset to a dictionary with iso_code as keys for quick lookups
   useEffect(() => {
@@ -24,6 +28,15 @@ const CovidWorldMap: React.FC<CovidWorldMapProps> = ({ data }) => {
     dataMap[data.iso_code] = data.total_cases;
     setCountryData(dataMap);
   }, [data]);
+
+  const handleMouseEnter = (event: React.MouseEvent, countryData: CovidCountryData) => {
+    setTooltipContent(`Country: ${countryData.location}, Cases: ${countryData.total_cases}`);
+    setMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handleMouseLeave = () => {
+    //setTooltipContent("");
+  };
 
   return (
     <div style={{ width: "100%", maxWidth: "800px", margin: "0 auto" }}>
@@ -39,13 +52,13 @@ const CovidWorldMap: React.FC<CovidWorldMapProps> = ({ data }) => {
                   key={geo.rsmKey}
                   geography={geo}
                   fill={colorScale(cases)}
-                  onMouseEnter={() => {
-                    console.log(`${geo.properties.name}: ${cases} cases`);
-                  }}
+                  
+                  onMouseEnter={(event) => handleMouseEnter(event, data)}
+                  onMouseLeave={handleMouseLeave}
                   style={{
                     default: { outline: "none" },
                     hover: { fill: "#FF5722", outline: "none" },
-                    pressed: { outline: "none" }
+                    pressed: { fill: "#E42", outline: "none" },
                   }}
                 />
               );
@@ -53,6 +66,18 @@ const CovidWorldMap: React.FC<CovidWorldMapProps> = ({ data }) => {
           }
         </Geographies>
       </ComposableMap>
+      <Tooltip
+        open={!!tooltipContent}
+        title={<Typography>{tooltipContent}</Typography>}
+        placement="top"
+        style={{
+          position: "absolute",
+          left: mousePosition.x + 15,
+          top: mousePosition.y + 15,
+        }}
+      >
+        <div />
+      </Tooltip>
     </div>
   );
 };
