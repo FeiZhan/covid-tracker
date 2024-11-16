2267f8d8fb74e2dc4fda970bdd208c934f4d84d9
Fei Zhan <fei.zhan.zju@gmail.com>
2024-11-15 16:49:00 -0800

    Add error handling to app

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,13 +5,15 @@ import covidRoutes from './routes/covidRoutes';
 const app = express();
 
 app.use(cors());
-// Middleware to handle JSON responses
 app.use(express.json());
 
-// Use the routes
 app.use('/api', covidRoutes);
 
-// Handle 404s and errors
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal server error' });
+});
+
 app.use((req, res) => {
   res.status(404).json({ message: 'Route not found' });
 });
